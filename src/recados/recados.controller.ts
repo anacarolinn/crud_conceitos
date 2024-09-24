fdc29b69543f3a6af03d9ee9285ede830cb3e318
Ana Carolina <anamachadodev@gmail.com>
2024-09-23 22:45:46 -0300

    status code

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
+  @HttpCode(HttpStatus.OK)
   // essa rota encontrar todos os recados
   // /recados/
   @Get()
@@ -11,8 +20,13 @@ export class RecadosController {
 
   // essa rota encontra um recado
   // /recados/:id/
-  @Get('fixa/:dinamico/:id')
-  findOne() {
-    return 'Essa rota retorna UM recado';
+  @Get(':id')
+  findOne(@Param('id') id: any) {
+    return `Essa rota retorna o recado ID ${id}`;
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return body;
   }
 }
