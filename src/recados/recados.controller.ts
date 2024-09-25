942d2715ef794eacb07468b41b52dd8fc101cf74
Ana Carolina <anamachadodev@gmail.com>
2024-09-24 22:21:53 -0300

    Usando o .service

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -10,40 +10,41 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { RecadosService } from './recados.service';
 
 @Controller('recados')
 export class RecadosController {
+  constructor(private readonly recadosService: RecadosService) {}
+
   @HttpCode(HttpStatus.OK)
   // essa rota encontrar todos os recados
   // /recados/
   @Get()
   findAll(@Query() pagination: any) {
     const { limit = 10, offset = 0 } = pagination;
-    return `Retorna todos os recados. Limit=${limit} e Offset=${offset}.`;
+    // return `Retorna todos os recados. Limit=${limit} e Offset=${offset}.`;
+    return this.recadosService.findAll();
   }
 
   // essa rota encontra um recado
   // /recados/:id/
   @Get(':id')
   findOne(@Param('id') id: any) {
-    return `Essa rota retorna o recado ID ${id}`;
+    return this.recadosService.findOne(id);
   }
 
   @Post()
   create(@Body() body: any) {
-    return body;
+    return this.recadosService.create(body);
   }
 
   @Patch(':id')
   updated(@Param('id') id: any, @Body() body: any) {
-    return {
-      id,
-      ...body,
-    };
+    this.recadosService.update(id, body);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: any, @Body() body: any) {
-    return `Essa rota apaga o ID ${id}`;
+  remove(@Param('id') id: any) {
+    this.recadosService.remove(id);
   }
 }
--- src/recados/recados.service.ts
+++ src/recados/recados.service.ts
@@ -1,8 +1,61 @@
 import { Injectable } from '@nestjs/common';
+import { Recado } from './entities/recado.entity';
 
 @Injectable()
 export class RecadosService {
-  getRecado(): string {
-    return 'recados';
+  private lastId = 1;
+  private recados: Recado[] = [
+    {
+      id: 1,
+      texto: 'Este é um recado de teste',
+      de: 'Clebersp',
+      para: 'João',
+      lido: false,
+      data: new Date(),
+    },
+  ];
+
+  findAll() {
+    return this.recados;
+  }
+
+  findOne(id: string) {
+    return this.recados.find(item => item.id === +id); // o sinal de + antes de id converte ele para number, já que aqui ele ta sendo como uma sting
+  }
+
+  create(body: any) {
+    this.lastId++;
+    const id = this.lastId;
+    const novoRecado = {
+      id,
+      ...body,
+    };
+    this.recados.push(novoRecado);
+    return novoRecado;
+  }
+
+  update(id: string, body: any) {
+    const recadoExistenteIndex = this.recados.findIndex(
+      item => item.id === +id,
+    );
+
+    if (recadoExistenteIndex >= 0) {
+      const recadoExistente = this.recados[recadoExistenteIndex];
+
+      this.recados[recadoExistenteIndex] = {
+        ...recadoExistente,
+        ...body,
+      };
+    }
+  }
+
+  remove(id: string) {
+    const recadoExistenteIndex = this.recados.findIndex(
+      // o findIndex é um método de procura do indice
+      item => item.id === +id,
+    );
+    if (recadoExistenteIndex >= 0) {
+      this.recados.splice(recadoExistenteIndex, 1);
+    }
   }
 }
