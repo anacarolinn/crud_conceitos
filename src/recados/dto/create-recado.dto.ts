b3944d51838bb29eead94f0f7c59d9b3ee3299bc
Ana Carolina <anamachadodev@gmail.com>
2024-10-02 20:55:07 -0300

    Validando dados

--- src/recados/dto/create-recado.dto.ts
+++ src/recados/dto/create-recado.dto.ts
@@ -1,4 +1,9 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateRecadoDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
   readonly texto: string; // o readonly é uma boa prática
   readonly de: string;
   readonly para: string;
