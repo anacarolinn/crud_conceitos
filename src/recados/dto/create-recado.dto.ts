36a0430290552da853a207fc748fee8fd7362c92
Ana Carolina <anamachadodev@gmail.com>
2024-10-02 21:29:37 -0300

    Bloqueando que usuários envie campos que não serão usados, `whitelisted` e para mensagens `forbidNonWhitelisted`

--- src/recados/dto/create-recado.dto.ts
+++ src/recados/dto/create-recado.dto.ts
@@ -1,10 +1,18 @@
-import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
 
 export class CreateRecadoDto {
   @IsString()
   @IsNotEmpty()
   @MinLength(5)
   readonly texto: string; // o readonly é uma boa prática
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
   readonly de: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
   readonly para: string;
 }
