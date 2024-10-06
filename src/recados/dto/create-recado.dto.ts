617f0589f8b4c8394985ccb245b80fcebe0790fa
Ana Carolina <anamachadodev@gmail.com>
2024-10-06 14:14:22 -0300

    Relações ManyToOne e OneToMany entre Recado e Pessoa

--- src/recados/dto/create-recado.dto.ts
+++ src/recados/dto/create-recado.dto.ts
@@ -1,18 +1,8 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateRecadoDto {
   @IsString()
   @IsNotEmpty()
   @MaxLength(255)
   readonly texto: string; // o readonly é uma boa prática
-
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(2)
-  readonly de: string;
-
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(2)
-  readonly para: string;
 }
--- src/recados/entities/recado.entity.ts
+++ src/recados/entities/recado.entity.ts
@@ -1,7 +1,10 @@
+import { Pessoa } from 'src/pessoas/entities/pessoa.entity';
 import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
@@ -14,12 +17,6 @@ export class Recado {
   @Column({ type: 'varchar', length: 255 }) // poderia passar algumas informações a mais de tipagem, mas se eu não declarar nada vai ser só mais uma coluna de texto
   texto: string;
 
-  @Column({ type: 'varchar', length: 50 })
-  de: string;
-
-  @Column({ type: 'varchar', length: 50 })
-  para: string;
-
   @Column({ default: false })
   lido: boolean;
 
@@ -31,4 +28,16 @@ export class Recado {
 
   @UpdateDateColumn()
   updateddAt?: Date;
+
+  // muitos recados podem ser enviados por uma única pessoa (emissor)
+  @ManyToOne(() => Pessoa)
+  // Especifica a coluna "de" que armazena o ID da pessoa que enviou o recado
+  @JoinColumn({ name: 'de' })
+  de: Pessoa;
+
+  // muitos recados podem ser enviados por uma única pessoa (destinário)
+  @ManyToOne(() => Pessoa)
+  // Especifica a coluna "para" que armazena o ID da pessoa que recebe o recado
+  @JoinColumn({ name: 'para' })
+  para: string;
 }
